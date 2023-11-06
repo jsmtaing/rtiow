c28fe3576c1dc42611fc726ad5e0063bcf7e007b
JOSHUA S. M. TAING <JOSTAI2003@GMAIL.COM>
2023-11-06 17:57:45 -0500

    Got rid of shadow acne problem

--- src/vec3.h
+++ src/vec3.h
@@ -104,7 +104,7 @@ inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
 
-inline vec3 random_in_unit_square() {
+inline vec3 random_in_unit_sphere() {
     while (true) {
         auto p = vec3::random(-1, 1);
         if (p.length_squared() < 1)
