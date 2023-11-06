4416e843ff1875765ab1c7acfcc99b7af8fba83f
JOSHUA S. M. TAING <JOSTAI2003@GMAIL.COM>
2023-11-06 18:28:24 -0500

    New lambertian material class

--- src/material.h
+++ src/material.h
@@ -14,4 +14,20 @@ class material {
       ) const = 0;
 };
 
+class lambertian : public material {
+    public:
+      lambertian(const color& a) : albedo(a) {}
+
+      bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scatter)
+      const override {
+        auto scatter_direction = rec.normal + random_unit_vector();
+        scattered = ray(rec.p, scatter_direction);
+        attenuation = albedo;
+        return true;
+      }
+
+    private:
+      color albedo;
+}
+
 #endif
