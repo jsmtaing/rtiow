65195f7a7c645943de1a955fe641400e957abeb8
JOSHUA S. M. TAING <JOSTAI2003@GMAIL.COM>
2023-11-06 19:39:18 -0500

    Scene with metal spheres

--- src/material.h
+++ src/material.h
@@ -18,7 +18,7 @@ class lambertian : public material {
     public:
       lambertian(const color& a) : albedo(a) {}
 
-      bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scatter)
+      bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
       const override {
         auto scatter_direction = rec.normal + random_unit_vector();
 
@@ -33,6 +33,22 @@ class lambertian : public material {
 
     private:
       color albedo;
-}
+};
+
+class metal : public material {
+    public:
+      metal(const color& a) : albedo(a) {}
+
+      bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+      const override {
+        vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+        scattered = ray(rec.p, reflected);
+        attenuation = albedo;
+        return true;
+      }
+
+    private:
+      color albedo;
+};
 
 #endif
