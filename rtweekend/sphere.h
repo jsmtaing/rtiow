41c3720f78ea5810c25c54c18aeb7ba5153a97c7
JOSHUA S. M. TAING <JOSTAI2003@GMAIL.COM>
2023-12-05 15:02:38 -0500

    Adding moving spheres

--- rtweekend/sphere.h
+++ rtweekend/sphere.h
@@ -6,40 +6,57 @@
 
 class sphere : public hittable {
     public:
+        // Stationary Sphere
         sphere(point3 _center, double _radius, shared_ptr<material> _material)
-         : center(_center), radius(_radius), mat(_material) {}
-
-        bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-        vec3 oc = r.origin() - center;
-        auto a = r.direction().length_squared();
-        auto half_b = dot(oc, r.direction());
-        auto c = oc.length_squared() - radius*radius;
-
-        auto discriminant = half_b*half_b - a*c;
-        if (discriminant < 0) return false;
-        auto sqrtd = sqrt(discriminant);
-
-        // Find the nearest root that lies in the acceptable range.
-        auto root = (-half_b - sqrtd) / a;
-        if (!ray_t.surrounds(root)) {
-            root = (-half_b + sqrtd) / a;
-            if (!ray_t.surrounds(root))
-                return false;
+         : center(_center), radius(_radius), mat(_material), is_moving(false) {}
+        
+        // Moving Sphere
+        sphere(point3 _center1, point3 _center2, double _radius, shared_ptr<material> _material)
+         : center1(_center1), radius(_radius), mat(_material), is_moving(true)
+        {
+            center_vec = _center2 - _center1;
         }
 
-        rec.t = root;
-        rec.p = r.at(rec.t);
-        vec3 outward_normal = (rec.p - center) / radius;
-        rec.set_face_normal(r, outward_normal);
-        rec.mat = mat;
+        bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+            point3 center = is_moving ? sphere_center(r.time()) : center1;
+            vec3 oc = r.origin() - center;
+            auto a = r.direction().length_squared();
+            auto half_b = dot(oc, r.direction());
+            auto c = oc.length_squared() - radius*radius;
 
-        return true;
-    }
+            auto discriminant = half_b*half_b - a*c;
+            if (discriminant < 0) return false;
+            auto sqrtd = sqrt(discriminant);
+
+            // Find the nearest root that lies in the acceptable range.
+            auto root = (-half_b - sqrtd) / a;
+            if (!ray_t.surrounds(root)) {
+                root = (-half_b + sqrtd) / a;
+                if (!ray_t.surrounds(root))
+                    return false;
+            }
+
+            rec.t = root;
+            rec.p = r.at(rec.t);
+            vec3 outward_normal = (rec.p - center) / radius;
+            rec.set_face_normal(r, outward_normal);
+            rec.mat = mat;
+
+            return true;
+        }
 
     private:
-        point3 center;
+        point3 center1;
         double radius;
         shared_ptr<material> mat;
+        bool is_moving;
+        vec3 center_vec;
+
+        point3 center(double time) const {
+            // Linearly interpolates from center1 to center2 according to time, where t = 0 yields
+            // center1, and t = 1 yields center2.
+            return center0 + time * center_vec;
+        }
 };
 
 #endif
