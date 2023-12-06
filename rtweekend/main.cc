49e947fd5d060c81789715f7c1eac7284bd375a4
JOSHUA S. M. TAING <JOSTAI2003@GMAIL.COM>
2023-12-06 16:52:06 -0500

    Motion blur final result

--- rtweekend/main.cc
+++ rtweekend/main.cc
@@ -54,7 +54,7 @@ int main() {
 
     cam.aspect_ratio      = 16.0 / 9.0;
     cam.image_width       = 400;
-    cam.samples_per_pixel = 100;
+    cam.samples_per_pixel = 5;
     cam.max_depth         = 50;
 
     cam.vfov     = 20;
